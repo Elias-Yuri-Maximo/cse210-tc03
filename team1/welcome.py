d1d258e56f8e6fdea8ac2a10b871ab193b9db6f1
Layne <nclhowell@gmail.com>
2021-09-30 08:27:49 -0700

    laynes changes

--- team1/welcome.py
+++ team1/welcome.py
@@ -1,9 +1,10 @@
-#from elias.py import hello_from_elias
+#from layne import hello_from_layne
 
 def main ():
     print("Welcome to our Collaborative Program")
-
-#hello_from_elias()
+    
+#hello_from_layne()
+    print_hello_from_layne()
 
 if __name__ == "__main__":
     main()
