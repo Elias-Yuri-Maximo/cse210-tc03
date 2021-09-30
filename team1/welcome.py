8c4cdf488846255afe73456092ba0385ab830aa6
NicolasPariente <par21022@byui.edu>
2021-09-30 12:22:15 -0300

    hera are nicolas changes

--- team1/welcome.py
+++ team1/welcome.py
@@ -1,9 +1,12 @@
 #from elias.py import hello_from_elias
+from pariente import hello_nicolas
 
-def main ():
+
+def main():
     print("Welcome to our Collaborative Program")
+    hello_nicolas()
+# hello_from_elias()
 
-#hello_from_elias()
 
 if __name__ == "__main__":
     main()
