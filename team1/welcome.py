dc26bf60eb31a114265a0e291fdc1b75f69c9a68
Elias-Yuri-Maximo <eliasmaximus@gmail.com>
2021-09-30 12:24:27 -0300

    Hello from Elias

--- team1/welcome.py
+++ team1/welcome.py
@@ -1,9 +1,11 @@
-#from elias.py import hello_from_elias
+from maximo import print_hello_from_elias
 
 def main ():
     print("Welcome to our Collaborative Program")
 
-#hello_from_elias()
+    print_hello_from_elias()
+    
+
 
 if __name__ == "__main__":
     main()
