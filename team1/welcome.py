4ee969c9a9eaf2a80719337cbfab33dc41a4cebf
unknown <sumtwelve@gmail.com>
2021-10-02 13:41:04 -0600

    noah's third attempt to commit python changes from noah-bio branch

--- team1/welcome.py
+++ team1/welcome.py
@@ -3,6 +3,7 @@
 from pariente import hello_nicolas
 from layne import print_hello_from_layne
 from maximo import print_hello_from_elias
+from nielsen import print_hello_from_noah
 
 
 
@@ -15,7 +16,7 @@ def main():
     hello_nicolas()
     print_hello_from_elias()
     print_hello_from_layne()
-
+    print_hello_from_noah
 
 
 if __name__ == "__main__":
